4679c3ceae2df52f22cb615d610816bdf1ddce88
Aniket Maurya <aniket.maurya@gdn-commerce.com>
2020-10-24 16:00:24 +0530

    chexnet

---
+++ src/chexnet.py
@@ -0,0 +1,67 @@
+# encoding: utf-8
+
+"""
+The main CheXNet model implementation.
+"""
+
+
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from sklearn.metrics import roc_auc_score
+
+
+CKPT_PATH = 'model.pth.tar'
+N_CLASSES = 14
+CLASS_NAMES = [ 'Atelectasis', 'Cardiomegaly', 'Effusion', 'Infiltration', 'Mass', 'Nodule', 'Pneumonia',
+                'Pneumothorax', 'Consolidation', 'Edema', 'Emphysema', 'Fibrosis', 'Pleural_Thickening', 'Hernia']
+DATA_DIR = './ChestX-ray14/images'
+TEST_IMAGE_LIST = './ChestX-ray14/labels/test_list.txt'
+BATCH_SIZE = 64
+
+
+def compute_AUCs(gt, pred):
+    """Computes Area Under the Curve (AUC) from prediction scores.
+
+    Args:
+        gt: Pytorch tensor on GPU, shape = [n_samples, n_classes]
+          true binary labels.
+        pred: Pytorch tensor on GPU, shape = [n_samples, n_classes]
+          can either be probability estimates of the positive class,
+          confidence values, or binary decisions.
+
+    Returns:
+        List of AUROCs of all classes.
+    """
+    AUROCs = []
+    gt_np = gt.cpu().numpy()
+    pred_np = pred.cpu().numpy()
+    for i in range(N_CLASSES):
+        AUROCs.append(roc_auc_score(gt_np[:, i], pred_np[:, i]))
+    return AUROCs
+
+
+class DenseNet121(nn.Module):
+    """Model modified.
+
+    The architecture of our model is the same as standard DenseNet121
+    except the classifier layer which has an additional sigmoid function.
+
+    """
+    def __init__(self, out_size):
+        super(DenseNet121, self).__init__()
+        self.densenet121 = torchvision.models.densenet121(pretrained=True)
+        num_ftrs = self.densenet121.classifier.in_features
+        self.densenet121.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, out_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.densenet121(x)
+        return x
